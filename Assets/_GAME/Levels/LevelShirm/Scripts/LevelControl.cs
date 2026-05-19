76fcf3c6db6c6881252ca6555204f1c747d4f364
sonerror <nguyenvansonawm@gmail.com>
2026-05-19 08:28:59 +0700

    add asset

--- Assets/_GAME/Levels/LevelShirm/Scripts/LevelControl.cs
+++ Assets/_GAME/Levels/LevelShirm/Scripts/LevelControl.cs
@@ -154,8 +154,6 @@ public class LevelControl : LevelBase
     }
     private void CheckDoneStep1()
     {
-        // DoneStep();
-        // TryNextStep();
         GameManager.Ins.showEndGame();
     }
 }
